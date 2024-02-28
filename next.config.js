46f2989ced2237288afe2438736e06ea73dced65
emmajane1313 <admin@digitalax.xyz>
2024-02-28 03:37:02 -0500

    studio landing

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@ const webpack = require("webpack");
 /** @type {import('next').NextConfig} */
 
 const allowedOrigins = [
-  "https://api.lens.dev",
   "https://thedial.infura-ipfs.io",
-  "https://vimeo.com",
-  "https://arweave.net/",
-  "https://api-v2.lens.dev/",
 ];
 
 const nextConfig = {
@@ -17,41 +13,13 @@ const nextConfig = {
     optimizeCss: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      net: false,
-      tls: false,
-      buffer: require.resolve("buffer/"),
-    };
-
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-      })
-    );
-
-    return config;
   },
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "chromadin.infura-ipfs.io",
-        pathname: "/ipfs/**",
-      },
-      {
-        protocol: "https",
         hostname: "thedial.infura-ipfs.io",
-        pathname: "/ipfs/**",
-      },
-      {
-        protocol: "https",
-        hostname: "gw.ipfs-lens.dev",
         pathname: "/ipfs/**",
       },
     ],
